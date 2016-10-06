af21e7d3d7e02c996e188b0add4a1110b45462ee
Marcus Schönning <marcus.schonning@chas.se>
2016-10-06 20:50:56 +0200

    mirrored server

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-var express = require('express');
+var express =  require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use(express.static(__dirname));
+
+app.set('views', __dirname);
+app.engine('html', require('ejs').renderFile);
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('index.html');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8080, 'localhost', function() {
+  console.log('Server port 8080');
 });
