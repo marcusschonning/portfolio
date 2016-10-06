a49c7d0c2a86e165634f10bf14c746b467659a1a
Marcus Schönning <marcus.schonning@chas.se>
2016-10-06 23:28:33 +0200

    added favicon

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express =  require('express');
 var app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/app'));
 
 app.set('views', __dirname);
 app.engine('html', require('ejs').renderFile);
@@ -9,7 +9,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-  res.render('index.html');
+  res.render('app/index.html');
 });
 
 app.listen(8080, 'localhost', function() {
